0a3a7e0e6ae47f9efaf84240474bd9ca954c2f13
shikha kumar <test.ckln@gmail.com>
2024-11-05 17:36:21 +0300

    Landing page updates + homepage fixes

---
+++ web/modules/custom/find_your_house/js/find-your-house.js
@@ -0,0 +1,11 @@
+(function ($) {
+  'use strict';
+  Drupal.behaviors.find_your_house = {
+      attach: function (context, settings) {
+          jQuery("#block-penchas-findyourhouse .select-menu ul.options li").on("click", function (event) {
+            jQuery('select#customer_find_your_house_group').val(jQuery(this).find('span').attr('data-group-url'));
+          });
+      }
+  };
+
+}(jQuery));
