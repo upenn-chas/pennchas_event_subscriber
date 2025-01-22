27df376bd1d8834eda44dac1f993e20fbaed6e21
Devteam <devteam@electrickite.com>
2025-01-22 13:14:52 +0000

    Added reports

--- web/modules/custom/pennchas_form_alter/src/Hook/NodePreUpdateHook.php
+++ web/modules/custom/pennchas_form_alter/src/Hook/NodePreUpdateHook.php
@@ -2,8 +2,12 @@
 
 namespace Drupal\pennchas_form_alter\Hook;
 
+use Drupal\block\Entity\Block;
+use Drupal\Core\Entity\EntityRepository;
 use Drupal\group\Entity\Group;
 use Drupal\group\Entity\GroupRelationship;
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
 use Drupal\pennchas_form_alter\Hook\Trait\EntityHookTrait;
@@ -18,6 +22,10 @@ class NodePreUpdateHook
         $nodeType = $node->bundle();
         if ($nodeType === Constant::NODE_EVENT) {
             $this->handleEvent($node);
+        }
+
+        if ($node->hasField('layout_builder__layout') && $node->hasField('field_blocks_ref')) {
+            $this->handleLayoutBuilder($node);
         }
     }
 
@@ -40,5 +48,46 @@ class NodePreUpdateHook
         $node->field_groups =  $housesId;
         $this->updateEventEndsOn($node);
         $node->set('field_is_campus_wide', count($node->get('field_groups')->getValue()) >= 14);
+    }
+
+    protected function handleLayoutBuilder(Node $node)
+    {
+        $layoutBuilder = $node->get('layout_builder__layout')->getValue();
+        $blocksIds = $this->getVideoBlocks($this->extractBlocks($layoutBuilder));
+        $node->set('field_blocks_ref', $blocksIds ?? null);
+    }
+
+    protected function extractBlocks(array $layout)
+    {
+        $blocks = [];
+        foreach ($layout as $ele) {
+            $section = $ele['section'];
+            $components = $section->getComponents();
+            foreach ($components as $comp) {
+                $pluginId = $comp->getPluginId();
+                $pluginId = explode(':', $pluginId);
+                if (count($pluginId) === 2 && $pluginId[0] === 'block_content') {
+                    $blocks[$pluginId[1]] = 1;
+                }
+            }
+        }
+        return array_keys($blocks);
+    }
+
+    protected function getVideoBlocks(array $blocksUuids)
+    {
+        if (!count($blocksUuids)) {
+            return [];
+        }
+        $blocksIds = [];
+        $blocks = \Drupal::entityTypeManager()
+            ->getStorage('block_content')
+            ->loadByProperties(['uuid' => $blocksUuids]);
+        foreach ($blocks as $block) {
+            if ($block->bundle() === 'video_block') {
+                $blocksIds[] = $block->id();
+            }
+        }
+        return $blocksIds;
     }
 }
---
+++ web/modules/custom/pennchas_form_alter/src/Plugin/FormAlter/UserFormAlter.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Drupal\pennchas_form_alter\Plugin\FormAlter;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\group\Entity\Group;
+use Drupal\group\Entity\GroupMembership;
+
+class UserFormAlter
+{
+    public function alter(array $form, FormStateInterface $formState)
+    {
+        $options = [
+            'senior_staff' => t('SeniorStaff'),
+            'ra' => t('RA'),
+            'student_worker' => t('Student Worker'),
+            'any_resident' => t('Any Resident'),
+            'anyone_with_pennkey' => t('Anyone with a Pennkey'),
+        ];
+        $roleType = $form['id']['#value'];
+
+
+        $form['umbera_roles'] = [
+            '#type' => 'checkboxes',
+            '#title' => t('Umbera Roles'),
+            '#description' => t('Choose an option from the dropdown list.'),
+            '#options' => $options,
+            '#default_value' => \Drupal::state()->get('custom_group_role_config_' . $roleType) ?: [],
+            '#required' => TRUE,
+        ];
+        $form['actions']['submit']['#submit'][] = [$this, 'roleSubmit'];
+        return $form;
+    }
+
+    public function roleSubmit(array $form, FormStateInterface $formState)
+    {
+        $selectedOptions = array_filter($formState->getValue('umbera_roles'));
+        $roleType = $form['id']['#value'];
+        \Drupal::state()->set('custom_group_role_config_' . $roleType, $selectedOptions);
+    }
+
+    function role_form_submit_handler(&$form, FormStateInterface $formState)
+    {
+        $umberaRoles = $formState->getValue('umbera_roles');
+        $umberaRoles = array_filter($umberaRoles);
+        if ($umberaRoles) {
+            $role = $formState->getFormObject()->getEntity();
+            $role->setThirdPartySetting('pennchas_form_alter', 'umbera_roles', $umberaRoles);
+            $role->save();
+        }
+    }
+}
