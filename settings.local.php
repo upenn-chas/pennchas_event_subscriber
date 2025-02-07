36d32525d10366d8a785e0f6d98ebe7968afb0fe
Rohit K <rohit@weassemble.team>
2025-02-07 05:57:53 +0000

    Added setting local in git inore file

--- settings.local.php
+++
@@ -1,8 +0,0 @@
-<?php
-$config['recaptcha_v3.settings']['site_key'] = '6LeJHJ8qAAAAAApHFIjYa8lypKlaVbf1RxsgdzCj';
-$config['recaptcha_v3.settings']['secret_key'] = '6LeJHJ8qAAAAALyeRIub9_k4cmszXvAPOkV2Azc6';
-
-$config['symfony_mailer.mailer_transport.smtp']['configuration']['user'] = 'smtp-relay/collegehouses.upenn.edu';
-$config['symfony_mailer.mailer_transport.smtp']['configuration']['pass'] = 'oktad;Kneb63GlovUthid';
-$config['symfony_mailer.mailer_transport.smtp']['configuration']['host'] = 'smtp-relay.upenn.edu';
-$config['symfony_mailer.mailer_transport.smtp']['configuration']['port'] = 25;
