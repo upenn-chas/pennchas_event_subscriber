5865bc3e3ec79bba42e062df70fa6c32f9f8e703
dev.ckln <dev.ckln@gmail.com>
2025-02-17 12:24:46 +0000

    UI changes

--- web/themes/custom/penchas/js/new_custom.js
+++ web/themes/custom/penchas/js/new_custom.js
@@ -1,28 +1,24 @@
 jQuery(document).ready(function () {
-    function toggleRadioOptions($this) {
-        var checkbox = $this;
+    function toggleRadioOptions(checkbox) {
         var field_is_campus_wide_value = jQuery('#edit-field-is-campus-wide-value').val();
         var field_intended_audience_value = jQuery('#edit-field-intended-audience-value').val();
         var currentUrlWithoutParams = window.location.origin + window.location.pathname;
 
         var type;
         if (checkbox.prop('checked')) {
-        // var type = jQuery('#edit-type-notices').val();
-        var type = jQuery('#edit-type-chas-event').val();
+            type = jQuery('#edit-type-chas-event').val();
         } else {
-        // var type = jQuery('#edit-type-chas-event').val();
-        var type = jQuery('#edit-type-notices').val();
+            type = jQuery('#edit-type-notices').val();
         }
-        // console.log(type);
-        console.log(currentUrlWithoutParams+'?type='+type+'&field_is_campus_wide_value='+field_is_campus_wide_value+'&field_intended_audience_value='+field_intended_audience_value);
-        var url = currentUrlWithoutParams+'?type='+type+'&field_is_campus_wide_value='+field_is_campus_wide_value+'&field_intended_audience_value='+field_intended_audience_value;
+        
+        console.log(type);
+        var url = currentUrlWithoutParams + '?type=' + type + '&field_is_campus_wide_value=' + field_is_campus_wide_value + '&field_intended_audience_value=' + field_intended_audience_value;
         return url;
     }
-        
-    // Call toggleRadioOptions on checkbox change
-    jQuery('#edit-type-exclude-notices').on('click',function() {
-        var url = toggleRadioOptions(jQuery(this).find('input'));
-        // window.location.href = url;
-    });
 
-})
+    // Simplified on click handler
+    jQuery('#edit-type-exclude-notices').on('change', function() {
+        var url = toggleRadioOptions(jQuery(this)); 
+        window.location.href = url;
+    });
+});
