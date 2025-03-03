7652311eb0ba873d6732580d86ca2401418f7d86
dev.ckln <dev.ckln@gmail.com>
2025-03-03 11:42:56 +0000

    UI changes

--- web/modules/custom/penchas_custom_token/src/Service/PendingModerationNodeCount.php
+++ web/modules/custom/penchas_custom_token/src/Service/PendingModerationNodeCount.php
@@ -33,13 +33,18 @@ class PendingModerationNodeCount
     // }
 
     public function nodeCountForAuthor(string $type, string $suffix = '') {
-        $userId = \Drupal::currentUser()->id();
+        $groups = \Drupal::service('pennchas_common.option_group')->getUserGroupsWithPermission('use editorial transition publish');
+
+        if (!count($groups)) {
+            return [];
+        }
+
         $query = \Drupal::database()->select('content_moderation_state_field_data', 'ms');
         $query->fields('ms', ['content_entity_id']);
-        $query->leftJoin('node_field_data', 'fs', 'fs.nid = ms.content_entity_id');
-        $query->condition('ms.uid', $userId, '=');
+        $query->leftJoin('node__field_groups', 'nfg', 'nfg.entity_id = ms.content_entity_id');
+        $query->condition('nfg.field_groups_target_id', array_keys($groups), 'IN');
         $query->condition('ms.moderation_state', ['draft', 'pending'], 'IN');
-        $query->condition('fs.type', $type);
+        $query->condition('nfg.bundle', 'chas_event', '=');
         $result = $query->distinct()->countQuery()->execute()->fetchCol();
         if($result[0] && $result[0] > 0) {
             return "{$result[0]} {$suffix}";
