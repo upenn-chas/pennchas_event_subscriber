3dc9e7cf002d6f1febd7a9ff3aa740591ce45cc4
dev.ckln <dev.ckln@gmail.com>
2025-03-11 08:28:28 +0000

    UI changes

--- web/modules/custom/penchas_block_group_role_condition/src/Plugin/Block/BlockCountForModerator.php
+++ web/modules/custom/penchas_block_group_role_condition/src/Plugin/Block/BlockCountForModerator.php
@@ -21,7 +21,7 @@ class BlockCountForModerator extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    // Initialize $group_role as FALSE
+
     $group_role = FALSE;
     
     $current_user = \Drupal::currentUser();
@@ -32,7 +32,7 @@ class BlockCountForModerator extends BlockBase {
       $roles = $membership->getRoles();
       foreach ($roles as $role) {
         if (in_array($role->id(), $required_roles)) {
-          $group_role = TRUE; // Correct assignment
+          $group_role = TRUE; 
         }
       }
     }
@@ -40,37 +40,57 @@ class BlockCountForModerator extends BlockBase {
     $user_roles = $current_user->getRoles();
     $allowed_roles = ['administrator', 'chas_director'];
 
-    // Check user roles or group role condition
     if (array_intersect($user_roles, $allowed_roles) || $group_role == TRUE) {
-        $groups = \Drupal::service('pennchas_common.option_group')->getUserGroupsWithPermission('use editorial transition publish');
+        // $groups = \Drupal::service('pennchas_common.option_group')->getUserGroupsWithPermission('use editorial transition publish');
 
+
+        $view = \Drupal\views\Views::getView('my_events');
+        $display_id = 'page_2'; 
+        $view->setDisplay($display_id);
+
+        $view->execute();
+        $total_results = $view->total_rows;
+
+        if (!empty($total_results)) {
+          return [
+              '#type' => 'inline_template',
+              '#template' => '<div class="info-fields"><p>You have {{ count }} events for moderation. Go to <a href="{{ goToUrl }}">My Events</a>.</p></div>',
+              '#context' => [
+                  'count' => $total_results,
+                  'goToUrl' => Url::fromRoute('view.my_events.page_2')->toString(),
+              ],
+              '#cache' => [
+                  'contexts' => ['user', 'session','user.roles'],
+              ],
+          ];
+        }
         // If no groups found, return an empty array to prevent passing null to the cacheable metadata
-        if (!count($groups)) {
-            return [];
-        }
+        // if (!count($groups)) {
+        //     return [];
+        // }
 
-        $query = \Drupal::database()->select('content_moderation_state_field_data', 'ms');
-        $query->fields('ms', ['content_entity_id']);
-        $query->leftJoin('node__field_groups', 'nfg', 'nfg.entity_id = ms.content_entity_id');
-        $query->condition('nfg.field_groups_target_id', array_keys($groups), 'IN');
-        $query->condition('ms.moderation_state', ['draft', 'pending'], 'IN');
-        $query->condition('nfg.bundle', 'chas_event', '=');
-        $result = $query->distinct()->countQuery()->execute()->fetchCol();
+        // $query = \Drupal::database()->select('content_moderation_state_field_data', 'ms');
+        // $query->fields('ms', ['content_entity_id']);
+        // $query->leftJoin('node__field_groups', 'nfg', 'nfg.entity_id = ms.content_entity_id');
+        // $query->condition('nfg.field_groups_target_id', array_keys($groups), 'IN');
+        // $query->condition('ms.moderation_state', ['draft', 'pending'], 'IN');
+        // $query->condition('nfg.bundle', 'chas_event', '=');
+        // $result = $query->distinct()->countQuery()->execute()->fetchCol();
 
-        // Check if we have content to render
-        if (!empty($result[0])) {
-            return [
-                '#type' => 'inline_template',
-                '#template' => '<div class="info-fields"><p>You have {{ count }} events for moderation. Go to <a href="{{ goToUrl }}">My Events</a>.</p></div>',
-                '#context' => [
-                    'count' => $result[0],
-                    'goToUrl' => Url::fromRoute('view.my_events.page_1')->toString(),
-                ],
-                '#cache' => [
-                    'contexts' => ['user', 'session'],
-                ],
-            ];
-        }
+        // // Check if we have content to render
+        // if (!empty($result[0])) {
+        //     return [
+        //         '#type' => 'inline_template',
+        //         '#template' => '<div class="info-fields"><p>You have {{ count }} events for moderation. Go to <a href="{{ goToUrl }}">My Events</a>.</p></div>',
+        //         '#context' => [
+        //             'count' => $result[0],
+        //             'goToUrl' => Url::fromRoute('view.my_events.page_1')->toString(),
+        //         ],
+        //         '#cache' => [
+        //             'contexts' => ['user', 'session'],
+        //         ],
+        //     ];
+        // }
     }
 
     // Ensure that you return an empty array if none of the conditions are met
