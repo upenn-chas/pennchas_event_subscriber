5ad131eadaf7ff5ca6ae9edd40173635e7c19a5a
dev.ckln <dev.ckln@gmail.com>
2025-03-20 06:31:41 +0000

    fixed sorting issue and menu links

---
+++ web/modules/custom/penchas_block_group_role_condition/src/EventSubscriber/LoadEventsSubscriber.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Drupal\penchas_block_group_role_condition\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Drupal\Core\Url;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoadEventsSubscriber implements EventSubscriberInterface {
+
+    /**
+     * {@inheritdoc}
+     *
+     * @return array
+     *   The event names to listen for, and the methods that should be executed.
+     */
+    public static function getSubscribedEvents() {
+        // $events[KernelEvents::REQUEST][] = array('checkForRedirection');
+        $events[KernelEvents::RESPONSE][] = ['onResponse',28];
+        return $events;
+    }
+
+    public function onResponse(ResponseEvent  $event) {
+    
+        $current_path = \Drupal::service('path.current')->getPath();
+        $route_match = \Drupal::routeMatch();
+        if (!\Drupal::currentUser()->isAuthenticated()) {
+            if("system.403" == $route_match->getRouteName()){
+                $login_url = '/saml_login?ReturnTo=' . urlencode($current_path);
+                $response = new RedirectResponse($login_url);
+                $event->setResponse($response);
+            }
+        }
+
+    }
+
+    // public function checkForRedirection(RequestEvent $event) {
+    //     // dump('checkForRedirection',$event);
+    // }
+
+}
