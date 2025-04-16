c945a991e5a1067e4d97af91185afcf1fa211ba4
Rohit K <rohit@weassemble.team>
2025-04-16 09:57:41 +0000

    Updated dashboard blocks

--- web/modules/custom/penchas_block_group_role_condition/src/TwigExtension.php
+++ web/modules/custom/penchas_block_group_role_condition/src/TwigExtension.php
@@ -39,54 +39,44 @@ class TwigExtension extends AbstractExtension
 
     public function verifyRoleAccess($field_values)
     {
-
         if (!isset($field_values['#items'])) {
             // If there are no field values, stop further processing.
             return;
         }
-        // dump($field_values);
-        $group_roles_arr = [];
-        foreach ($field_values['#items']->referencedEntities() as $value) {
-            $group_roles_arr[] = $value->id();
-        }
-
         $current_user = \Drupal::currentUser();
-        $user_roles = $current_user->getRoles();  // Get an array of the current user's roles
-
         // Skip processing if the current user is an admin
-        if (in_array('administrator', $user_roles)) {
-            return TRUE; // If the user is an admin, prevent role-based access
+        if ($current_user->hasRole('administrator')) {
+            return true;
+        }
+        $group_roles_arr = [];
+        foreach ($field_values['#items']->getValue() as $item) {
+            $group_roles_arr[$item['value']] = $item['value'];
         }
 
-        $group_role = TRUE;
-        $user_entity = \Drupal\user\Entity\User::load($current_user->id());
+        // $user_roles = $current_user->getRoles(TRUE);  // Get an array of the current user's roles
+        foreach($current_user->getRoles(TRUE) as $role) {
+            if(isset($group_roles_arr[$role])) {
+                return TRUE;
+            }
+        }
+        // Check if user has group roles.
         $connection = Database::getConnection();
-        $query = $connection->select('group_relationship_field_data', 'gr')
-            ->fields('gr', ['id'])
-            ->condition('entity_id', $current_user->id(), '=')
-            ->condition('plugin_id', 'group_membership', '=');
+        $query = $connection->select('group_relationship_field_data', 'grfd');
+        $query->innerJoin('group_relationship__group_roles', 'grgr', 'grfd.id = grgr.entity_id');
+        $query->fields('grgr', ['group_roles_target_id'])
+            ->condition('grfd.entity_id', $current_user->id(), '=')
+            ->condition('grfd.plugin_id', 'group_membership', '=');
 
         // Execute the query and fetch all results as an associative array.
-        $results = $query->execute()->fetchAssoc('id');
-        if (!empty($results['id'])) {
-            $current_user_role_id = $results['id'];
-            $query = $connection->select('group_relationship__group_roles', 'grr')
-                ->fields('grr', ['group_roles_target_id'])
-                ->condition('entity_id', $current_user_role_id, '=');
-            $group_roles_results = $query->execute()->fetchAssoc('group_roles_target_id');
-            if (!empty($group_roles_results)) {
-                $group_role = $group_roles_results['group_roles_target_id'];
+        $results = $query->distinct()->execute()->fetchAllAssoc('group_roles_target_id');
+        if(!empty($results)) {
+            foreach($results as $role => $data) {
+                if(isset($group_roles_arr[$role])) {
+                    return TRUE;
+                }
             }
         }
-        if (!empty($group_role)) {
-            $key = array_search($group_role, $group_roles_arr);
-            if ($key !== false) {
-                $group_role = TRUE;
-            } else {
-                $group_role = FALSE;
-            }
-        }
-        return $group_role;
+        return FALSE;
     }
 
     public function getGroupName()
