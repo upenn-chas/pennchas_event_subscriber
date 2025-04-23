87326c9cc80dc5b99ffe3c16472ae911954927f0
Rohit K <rohit@weassemble.team>
2025-04-23 07:24:28 +0000

    Fixed moderation count on dashboard

--- web/modules/custom/penchas_custom_token/src/Service/PendingModerationNodeCount.php
+++ web/modules/custom/penchas_custom_token/src/Service/PendingModerationNodeCount.php
@@ -2,8 +2,6 @@
 
 namespace Drupal\penchas_custom_token\Service;
 
-use Drupal\Core\StringTranslation\TranslatableMarkup;
-use Drupal\Core\Url;
 
 class PendingModerationNodeCount
 {
@@ -24,9 +22,9 @@ class PendingModerationNodeCount
     //     $result = $query->distinct()->countQuery()->execute()->fetchCol();
 
     //     if($result[0] && $result[0] > 0) {
-    //         return new TranslatableMarkup('<div class="info-fields"><p>You have @count events for moderation. Go to <a href="@goToUrl">My Events</a>.</p></div>', [
+    //         return new \Drupal\Core\StringTranslation\TranslatableMarkup('<div class="info-fields"><p>You have @count events for moderation. Go to <a href="@goToUrl">My Events</a>.</p></div>', [
     //             '@count' => $result[0],
-    //             '@goToUrl' => Url::fromRoute('view.my_events.page_1')->toString()
+    //             '@goToUrl' => \Drupal\Core\Url::fromRoute('view.my_events.page_1')->toString()
     //           ]);
     //     }
     //     return NULL;
@@ -40,16 +38,15 @@ class PendingModerationNodeCount
         // if (!count($groups)) {
         //     return "No {$suffix}";
         // }
-       
-        $query = \Drupal::database()->select('content_moderation_state_field_data', 'ms');
+
+        $query = \Drupal::database()->select('node_field_data', 'nfd');
+        $query->join('content_moderation_state_field_data', 'ms', 'nfd.vid = ms.content_entity_revision_id');
         $query->fields('ms', ['content_entity_id']);
-        $query->join('node_field_data', 'nf', 'nf.nid = ms.content_entity_id');
-        $query->join('node__field_event_schedule', 'nfes', 'ms.content_entity_id = nfes.entity_id');
-        // $query->condition('nfg.field_groups_target_id', array_keys($groups), 'IN');
+        $query->join('node__field_event_schedule', 'nfes', 'nfd.nid = nfes.entity_id');
         $query->condition('nfes.field_event_schedule_value', time(), '>');
-        $query->condition('ms.uid', $uid, '=');
+        $query->condition('nfd.type', $type, '=');
+        $query->condition('nfd.uid', $uid, '=');
         $query->condition('ms.moderation_state', ['draft', 'pending'], 'IN');
-        $query->condition('nf.type', $type, '=');
         $result = $query->distinct()->countQuery()->execute()->fetchCol();
         if(!empty($result)){
             if($result[0] && $result[0] > 0) {
