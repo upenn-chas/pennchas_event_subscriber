50d7150d77ebdafe25263ffb653637fe09a84258
Rohit K <rohit@weassemble.team>
2025-05-08 10:06:24 +0000

    Added new field for layout rendered part

--- web/modules/custom/pennchas_form_alter/src/Hook/Trait/HousePageHookTrait.php
+++ web/modules/custom/pennchas_form_alter/src/Hook/Trait/HousePageHookTrait.php
@@ -4,6 +4,8 @@ namespace Drupal\pennchas_form_alter\Hook\Trait;
 
 use Drupal\Core\Url;
 use Drupal\group\Entity\Group;
+use Drupal\layout_builder\Section;
+use Drupal\layout_builder\SectionComponent;
 use Drupal\node\Entity\Node;
 
 trait HousePageHookTrait
@@ -26,5 +28,33 @@ trait HousePageHookTrait
             }
         }
         $node->set('field_group_ref', $pageRef);
+        $this->renderLayoutBuilderContent($node);
+    }
+
+    protected function renderLayoutBuilderContent(Node $node)
+    {
+        $layoutBuilderContent = '';
+        if ($node->hasField('layout_builder__layout')) {
+            $layout = $node->get('layout_builder__layout')->getValue();
+            $build = [];
+
+            foreach ($layout as $sectionArr) {
+                $section = $sectionArr['section'];
+                foreach ($section->getComponents() as $component) {
+                    if ($component instanceof SectionComponent) {
+                        $build[] = $component->toRenderArray();
+                    }
+                }
+            }
+
+            $renderer = \Drupal::service('renderer');
+            $rendered_text = $renderer->renderPlain($build);
+            $plain_text = strip_tags($rendered_text);
+            $plain_text = preg_replace('/[^a-zA-Z0-9\s]/', '',preg_replace('/\s{2,}/', ' ', str_replace("\n", ' ', $plain_text)));
+
+            // Store in the text field.
+            $node->set('field_rendered_layout', $plain_text);
+        }
+        return $layoutBuilderContent;
     }
 }
