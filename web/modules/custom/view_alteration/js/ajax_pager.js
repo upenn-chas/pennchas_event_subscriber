e2a99164b5c51e561ef18d9d36673898523afa6f
Rohit K <rohit@weassemble.team>
2025-06-16 13:56:52 +0000

    Disable js

--- web/modules/custom/view_alteration/js/ajax_pager.js
+++ web/modules/custom/view_alteration/js/ajax_pager.js
@@ -2,13 +2,13 @@
   Drupal.behaviors.ajaxViewPager = {
     attach: function (context, settings) {
       // Check if the view block is in the layout and initialize AJAX pager.
-      $('.view-display-id-my_events_block .pager', context).once('ajax-view-pager').each(function () {
-        // Reattach AJAX pager for the view block.
-        var pager = $(this);
-        pager.find('a').each(function () {
-          $(this).attr('data-drupal-selector', 'ajax-pager-link');
-        });
-      });
+      // $('.view-display-id-my_events_block .pager', context).once('ajax-view-pager').each(function () {
+      //   // Reattach AJAX pager for the view block.
+      //   var pager = $(this);
+      //   pager.find('a').each(function () {
+      //     $(this).attr('data-drupal-selector', 'ajax-pager-link');
+      //   });
+      // });
     }
   };
 })(jQuery, Drupal);
